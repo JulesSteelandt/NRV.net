30f0a7f61d1f24dd9c32c09c1e4d1301682968d9
Nathan <nathan.melbeck@gmail.com>
2023-10-24 15:20:16 +0200

    feat: JwtManager.php -> validate()

---
+++ api/auth.nrv/src/app/auth/managers/JwtManager.php
@@ -0,0 +1,30 @@
+<?php
+
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
+use pizzashop\auth\api\domain\exception\JwtExpiredException;
+use pizzashop\auth\api\domain\exception\JwtInvalidException;
+
+class JwtManager {
+
+
+
+
+
+
+
+
+    public function validate(string $token): stdClass {
+        try {
+            return JWT::decode($token, new Key($_ENV['JWT_SECRET'], 'HS512'));
+        } catch (ExpiredException $e) {
+            throw new JwtExpiredException();
+        } catch (SignatureInvalidException $e) {
+            throw new JwtInvalidException('signature');
+        } catch (UnexpectedValueException $e) {
+            throw new JwtInvalidException('invalid');
+        }
+    }
+}
---
+++ api/auth.nrv/src/domain/exception/JwtExpiredException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace nrv\auth\domain\exception;
+
+use Exception;
+
+class JwtExpiredException extends Exception {
+
+}
