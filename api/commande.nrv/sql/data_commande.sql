af87c721edc18200015c0ae65a2e3a7b367badca
Sonii79 <sonij590833@gmail.com>
2023-10-24 13:36:44 +0200

    init: bdd auth et commande

---
+++ api/commande.nrv/sql/data_commande.sql
@@ -0,0 +1,14 @@
+INSERT INTO COMMANDE values ('user1@gmail.com',1,78.07);
+INSERT INTO COMMANDE values ('user2@gmail.com',2,39.94);
+INSERT INTO COMMANDE values ('user2@gmail.com',3,52.06);
+INSERT INTO COMMANDE values ('user2@gmail.com',3,22.68);
+
+INSERT INTO COMMANDE2SOIREE values (1,1,1,1);
+INSERT INTO COMMANDE2SOIREE values (1,2,1,1);
+
+INSERT INTO COMMANDE2SOIREE values (2,1,1,1);
+INSERT INTO COMMANDE2SOIREE values (3,3,1,2);
+INSERT INTO COMMANDE2SOIREE values (4,4,2,1);
+
+INSERT INTO BILLET values ('02ce0536-2091-4a97-bb8a-034bf1a0e92c',3,'user2@gmail.com','2023-10-27','20:00:00','Prix plein');
+INSERT INTO BILLET values ('5a9c37f8-b2e7-4d4d-8a8a-2269864b0b77',4,'user2@gmail.com','2023-10-28','21:00:00','Prix réduit');
