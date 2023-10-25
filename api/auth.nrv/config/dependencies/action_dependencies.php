d351f3125c657e7020c1a4f117e23ddf0c6bf2c9
Nathan <nathan.melbeck@gmail.com>
2023-10-25 18:42:39 +0200

    feat : Action refreshUser (pas ok)

--- api/auth.nrv/config/dependencies/action_dependencies.php
+++ api/auth.nrv/config/dependencies/action_dependencies.php
@@ -1,11 +1,16 @@
 <?php
 
 use nrv\auth\app\actions\SignInAction;
+use nrv\auth\app\actions\UserRefreshAction;
 use Psr\Container\ContainerInterface;
 
 return [
     SignInAction::class => function (ContainerInterface $c) {
         return new SignInAction($c->get('authenticate.service'));
     },
+
+    UserRefreshAction::class => function (ContainerInterface $c) {
+        return new UserRefreshAction($c->get('authenticate.service'));
+    }
 
 ];
--- api/auth.nrv/config/routes.php
+++ api/auth.nrv/config/routes.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 use nrv\auth\app\actions\SignInAction;
 
-return function(\Slim\App $app):void {
+return function (\Slim\App $app): void {
 
-    $app->post("/signin",SignInAction::class)->setName("signIn");
-
+    $app->post("/signin", SignInAction::class)->setName("signIn");
+    $app->post("/refresh", \nrv\auth\app\actions\UserRefreshAction::class)->setName("refreshUser");
 
 };
