ac5d12f97802cfac49edfa64716e94a35ac370bd
Nathan <nathan.melbeck@gmail.com>
2023-10-25 19:40:16 +0200

    feat : Action ValiderTokenJWTAction.php (ok)

--- api/auth.nrv/config/dependencies/action_dependencies.php
+++ api/auth.nrv/config/dependencies/action_dependencies.php
@@ -1,11 +1,16 @@
 <?php
 
 use nrv\auth\app\actions\SignInAction;
+use nrv\auth\app\actions\ValiderTokenJWTAction;
 use Psr\Container\ContainerInterface;
 
 return [
     SignInAction::class => function (ContainerInterface $c) {
         return new SignInAction($c->get('authenticate.service'));
+    },
+
+    ValiderTokenJWTAction::class => function (ContainerInterface $c) {
+        return new ValiderTokenJWTAction($c->get('authenticate.service'));
     },
 
 ];
