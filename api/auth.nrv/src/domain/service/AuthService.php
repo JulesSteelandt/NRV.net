044a914394c0db0e85576988d01adcd52c6f8c8b
Nathan <nathan.melbeck@gmail.com>
2023-10-25 11:20:37 +0200

    feat : service + interface

---
+++ api/auth.nrv/src/domain/service/AuthService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace nrv\auth\domain\service;
+
+use nrv\auth\app\auth\managers\JwtManager;
+use nrv\auth\app\auth\providers\AuthProvider;
+use nrv\auth\domain\dto\CredentialsDTO;
+use nrv\auth\domain\dto\TokenDTO;
+use nrv\auth\domain\dto\userDTO;
+use nrv\auth\domain\service\AuthServiceInterface;
+
+class AuthService implements AuthServiceInterface {
+    private JwtManager $jwtManager;
+    private AuthProvider $authProvider;
+
+    /**
+     * @param JwtManager $jwtManager
+     * @param AuthProvider $authProvider
+     */
+    public function __construct(JwtManager $jwtManager, AuthProvider $authProvider) {
+        $this->jwtManager = $jwtManager;
+        $this->authProvider = $authProvider;
+    }
+
+    public function signup(CredentialsDTO $credentialsDTO): UserDTO {
+        $this->authProvider->register($credentialsDTO->email, $credentialsDTO->password, $credentialsDTO->nom, $credentialsDTO->prenom);
+        $us = $this->authProvider->getAuthenticatedUser();
+        return new UserDTO($us['email'], $us['nom'], $us['prenom'], $us['typeUtil']);
+    }
+
+    public function signin(CredentialsDTO $credentialsDTO): TokenDTO {
+        $this->authProvider->checkCredentials($credentialsDTO->email,$credentialsDTO->password);
+        return $this->authProvider->genToken($this->authProvider->getUser($credentialsDTO->email,''), $this->jwtManager);    }
+
+    public function validate(TokenDTO $tokenDTO): UserDTO {
+            $payload = $this->jwtManager->validate($tokenDTO->jwt);
+        return new UserDTO($payload->upr->email, $payload->upr->nom, $payload->upr->prenom, $payload->upr->typeUtil);
+
+    }
+
+    public function refresh(TokenDTO $tokenDTO): TokenDTO {
+
+        $payload = $this->jwtManager->validate($tokenDTO->jwt);
+        $this->authProvider->checkToken($payload->upr->refresh_token);
+        return $this->authProvider->genToken($this->authProvider->getUser($payload->upr->email, $payload->upr->refresh_token), $this->jwtManager);
+    }
+}
